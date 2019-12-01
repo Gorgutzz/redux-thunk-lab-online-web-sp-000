9f0b5cd8bb4e1bbfa3d43c1adfa5966e0d8e211c
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-12-01 23:38:24 +0000

    Automatically backed up by Learn

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,11 +1,8 @@
 // write and export your action creator function here
 function fetchCats() {
-  const cats = [
-    {name: "Neil Armstrong", craft: "Apollo 11"},
-    {name: "Buzz Aldrin", craft: "Apollo 11"},
-    {name: "Michael Collins", craft: "Apollo 11"}
-  ];
-  return {
+  const cats = fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
+    return response.json()
+  }).then(responseJSON => {
     type: 'ADD_ASTRONAUTS',
     astronauts
   };
