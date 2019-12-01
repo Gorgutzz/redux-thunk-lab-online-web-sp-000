38146cfc61b9e089380929c0e7541cf20b7e304a
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-12-01 23:39:46 +0000

    Automatically backed up by Learn

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,5 +1,5 @@
 // write and export your action creator function here
-export const fetchCats = () => {
+function fetchCats() {
   const cats = fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
     return response.json()
   }).then(responseJSON => {
@@ -7,3 +7,14 @@ export const fetchCats = () => {
     astronauts
   };
 };
+
+export const fetchCats = () => {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_CATS'})
+    fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
+      return response.json()
+    }).then(responseJSON => {
+      dispatch({ type: 'ADD_CATS', cats: responseJSON.images })
+    })
+  }
+}
