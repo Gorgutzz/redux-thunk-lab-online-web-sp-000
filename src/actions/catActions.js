8daa76ac287b5bfa8623329517a936ef5b481e53
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-12-01 23:36:36 +0000

    Automatically backed up by Learn

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,21 +1,12 @@
 // write and export your action creator function here
-const catsReducer = (state = { cats: [], loading: false }, action) => {
-  switch(action.type) {
-    case 'LOADING_CATS':
-      return {
-        ...state,
-        cats: [...state.cats],
-        loading: true
-      }
-    case 'ADD_CATS':
-      return {
-        ...state,
-        cats: action.cats,
-        loading: false
-      }
-    default:
-      return state;
-  }
-}
-
-export default catsReducer;
+function fetchAstronauts() {
+  const astronauts = [
+    {name: "Neil Armstrong", craft: "Apollo 11"},
+    {name: "Buzz Aldrin", craft: "Apollo 11"},
+    {name: "Michael Collins", craft: "Apollo 11"}
+  ];
+  return {
+    type: 'ADD_ASTRONAUTS',
+    astronauts
+  };
+};
